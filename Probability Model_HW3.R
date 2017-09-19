f7a120a81883e0d2750ca9d74826f747997dfa3d
Piyush <piyushverma.iitkgp@gmail.com>
2017-09-19 15:08:22 -0400

    More changes

--- Probability Model_HW3.R
+++ Probability Model_HW3.R
@@ -35,9 +35,13 @@ pbinom(25,400,.07) - pbinom(20,400,.07) #00.2541306
 #Solution 6
 x<-rpois(100,3)
 y<-x^2
-
+dat<-data.frame(x,y)
 #a
-hist(y,main = "Histogram of y (1st part)",col = "blue",xlab = "Y", ylab = "Density")
+g<-ggplot(data = dat,aes(x=dat$y))+geom_histogram(binwidth = 1,col = "red")
+g<-g+ggtitle("Distribution of Y (1st part)")+theme(plot.title = element_text(hjust=0.5))+xlab("Y")+ylab("Frequency")
+g
+
+
 
 dt<-data.frame(y_bar=0,sd=0)  #Initializing an empty data frame
 for (i in 1:1000) {  #looping 1000 times to store y_bar in dt
@@ -48,44 +52,61 @@ sd<-sd(y)
 dt<-rbind(dt,cbind(y_bar,sd))
 }
 dt<-data.frame(dt[2:1001,])
-
 #b
-hist(dt$y_bar,col = "blue",density = 20, main = "Histogram of y_bar (1st part)") 
+g<-ggplot(data = dt,aes(x=dt$y))+geom_histogram(binwidth = 1,col = "red")
+g<-g+ggtitle("Distribution of Y_bar (1st part)")+theme(plot.title = element_text(hjust=0.5))+xlab("Y_bar")+ylab("Frequency")
+g
+
 
 u_bar<-mean(dt$y_bar)
 dt$z<-with(dt, (dt$y_bar - u_bar)/(dt$sd/10))
-hist(dt$z,density = 20,col = "blue", main = "Histogram of Z & fitted normal curve (1st part)",xlim = c(-4,3),ylim = c(0,0.45),probability = TRUE,xlab = "Z",ylab = "Density")
-par(new = TRUE)
-
+mean_z<-mean(dt$z)
+sd_z<-sd(dt$z)
 #c
-plot(seq(-4,4,by = 0.005),dnorm(seq(-4,4,by = 0.005)),xlim = c(-4,3),ylim = c(0,0.45),xlab = "Z",ylab = "Density")
+g<-ggplot(data = dt,aes(x=dt$z))+geom_histogram(binwidth = 1,col = "red")
+g<-g+ggtitle("Distribution of Z & Fitted normal curve (1st part)")+theme(plot.title = element_text(hjust=0.5))+xlab("Z")+ylab("Frequency")
+g<-g+stat_function(fun = function(x,mean_z,sd_z){
+dnorm(x=x,mean_z,sd_z)*1000},
+args = c(mean = mean_z, sd = sd_z)
+,size = 1.5,col = "blue")
+g
 
 
 #d
 
 x<-rexp(100,1/3)
 y<-x^2
+dat<-data.frame(x,y)
+g<-ggplot(data = dat,aes(x=dat$y))+geom_histogram(binwidth = 1,col = "red")
+g<-g+ggtitle("Distribution of Y (2nd part)")+theme(plot.title = element_text(hjust=0.5))+xlab("Y")+ylab("Frequency")
+g
 
 
-hist(y,main = "Histogram of Y (2nd part)", col = "red",xlab = "Y", ylab = "Density")
 
 dt<-data.frame(y_bar=0,sd=0)  #Initializing an empty data frame
 for (i in 1:1000) {  #looping 1000 times to store y_bar in dt
-  x<-rexp(100,1/3)
+  x<-rpois(100,3)
   y<-x^2
   y_bar<-mean(y)
   sd<-sd(y)
   dt<-rbind(dt,cbind(y_bar,sd))
 }
 dt<-data.frame(dt[2:1001,])
+g<-ggplot(data = dt,aes(x=dt$y))+geom_histogram(binwidth = 1,col = "red")
+g<-g+ggtitle("Distribution of Y_bar (2nd part)")+theme(plot.title = element_text(hjust=0.5))+xlab("Y_bar")+ylab("Frequency")
+g
 
-
-hist(dt$y_bar,col = "red",density = 20, main = "Histogram of y_bar (2nd part)") 
 
 u_bar<-mean(dt$y_bar)
 dt$z<-with(dt, (dt$y_bar - u_bar)/(dt$sd/10))
-hist(dt$z,density = 20,col = "red", main = "Histogram of Z & fitted normal curve (2nd part)",xlim = c(-4,3),ylim = c(0,0.45),probability = TRUE,xlab = "Z",ylab = "Density")
-par(new = TRUE)
-plot(seq(-4,4,by = 0.005),dnorm(seq(-4,4,by = 0.005)),xlim = c(-4,3),ylim = c(0,0.45),xlab = "Z",ylab = "Density")
+mean_z<-mean(dt$z)
+sd_z<-sd(dt$z)
+g<-ggplot(data = dt,aes(x=dt$z))+geom_histogram(binwidth = 1,col = "red")
+g<-g+ggtitle("Distribution of Z & Fitted normal curve (2nd part)")+theme(plot.title = element_text(hjust=0.5))+xlab("Z")+ylab("Frequency")
+g<-g+stat_function(fun = function(x,mean_z,sd_z){
+  dnorm(x=x,mean_z,sd_z)*1000},
+  args = c(mean = mean_z, sd = sd_z)
+  ,size = 1.5,col = "blue")
+g
 
 
